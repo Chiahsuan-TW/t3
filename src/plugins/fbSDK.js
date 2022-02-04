4ad56a784bd63c027da546e6c30b7e1c7ebc053a
Chiahsuan Chou <belinda05021992@gmail.com>
2022-02-04 09:37:58 +0800

    fix: error with invalid version

--- src/plugins/fbSDK.js
+++ src/plugins/fbSDK.js
@@ -1,4 +1,4 @@
-(function (d, s, id) {
+export const fbSDK = (function (d, s, id) {
   var js,
     fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {
@@ -6,6 +6,6 @@
   }
   js = d.createElement(s);
   js.id = id;
-  js.src = "//connect.facebook.net/zh_TW/sdk.js";
+  js.src = "//connect.facebook.net/zh_TW/all.js";
   fjs.parentNode.insertBefore(js, fjs);
 })(document, "script", "facebook-jssdk");
